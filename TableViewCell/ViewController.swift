3502bc83742df80bb73a7fe031e48c8e37e9aa04
im100ruv <karnasaurabh123@gmail.com>
2018-01-21 21:23:51 +0530

    Initial Commit

---
+++ TableViewCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewCell
+//
+//  Created by Im100ruv on 21/01/18.
+//  Copyright © 2018 Im100ruv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
